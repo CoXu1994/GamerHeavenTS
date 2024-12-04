51e7d97002793a6403391b8103bf10e3368a8f3a
CoXu1994 <mateusz.radomski1994@gmail.com>
2024-12-04 11:13:51 +0100

    Recreated navigation bar using MUI. Changed structure for mobile usage ( burger menu ). Basic Styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,30 +1,81 @@
+import {
+	AppBar,
+	Box,
+	Drawer,
+	IconButton,
+	Toolbar,
+	ListItemButton,
+	Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
+	const [open, toggleDrawer] = useState(false);
 	return (
-		<>
-			<div>
-				<Link to="/">
-					<span className="nav__title icon-gamepad">GamerHeaven</span>
-				</Link>
-				<div>
-					{/* {Mobile Navigation} */}
-					{/* <Link to="/wish-list">
-						<p>Go to wish list</p>
+		<Box>
+			<AppBar position="fixed">
+				<Toolbar>
+					<Link
+						to="/"
+						style={{ textDecoration: "none" }}
+					>
+						<Typography className="nav__title icon-gamepad">
+							GamerHeaven
+						</Typography>
 					</Link>
-					<Link to="categories">
-						<p>Go to categories</p>
-					</Link> */}
+					<IconButton
+						edge="start"
+						color="inherit"
+						aria-label="open drawer"
+						onClick={() => toggleDrawer(true)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Drawer
+						anchor="top"
+						variant="temporary"
+						open={open}
+						onClose={() => toggleDrawer(false)}
+					>
+						<Box>
+							<Link
+								to="/wishlist"
+								style={{ textDecoration: "none" }}
+							>
+								<ListItemButton>
+									<Typography>Go to wish list</Typography>
+								</ListItemButton>
+							</Link>
+							<Link
+								to="/categories"
+								style={{ textDecoration: "none" }}
+							>
+								<ListItemButton>
+									<Typography>Go to categories</Typography>
+								</ListItemButton>
+							</Link>
+						</Box>
+					</Drawer>
 
-					<Link to="/wishlist">
-						<p>Wish list</p>
-					</Link>
-					<Link to="/categories">
-						<p>Categories</p>
-					</Link>
-				</div>
-			</div>
-		</>
+					<Box>
+						<Link
+							to="/wishlist"
+							style={{ textDecoration: "none" }}
+						>
+							<Typography>Wish list</Typography>
+						</Link>
+						<Link
+							to="/categories"
+							style={{ textDecoration: "none" }}
+						>
+							<Typography>Categories</Typography>
+						</Link>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 };
 
